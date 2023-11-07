5476d0754c66126688994a6cd2bda93f520bf404
tuvantai123vn <nguyenthanhtu.cdct@gmail.com>
2023-11-07 15:51:52 +0700

    update7_11_6

--- src/Authentication/SignIn.jsx
+++ src/Authentication/SignIn.jsx
@@ -51,7 +51,6 @@ function SignIn(props) {
         const data = responseData.data;
         setCookie("accessToken", data, {maxAge: 7200});
           console.log('setcookies', cookies);
-          navigate("/");
 
         if (responseData.ok && responseData._id) {
           const action = addSession(localStorage.getItem("id_user"));
